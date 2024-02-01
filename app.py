88ceebab9e1122e4be915e8053f5c485be373307
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2024-02-01 17:13:22 +0100

    finish next_turn function.

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
 from tkinter import *
 import random
 
-def next_turn():
-    pass
+def next_turn(row, column):
+    global player
+    if buttons[row][column]["text"]  == "" and check_winner() is False:
+        if player == players[0]:
+            buttons[row][column]["text"] = player
+            if check_winner() is False:
+                player = players[1]
+                label.config(text=player[1] + " turn")
+            elif check_winner() is True:
+                label.config(text=players[0] + "wins")
+            elif check_winner() == "Tie":
+                label.config(text="Tie")
+        else:
+            buttons[row][column]["text"] = player
+            if check_winner() is False:
+                player = players[0]
+                label.config(text=player[0] + " turn")
+            elif check_winner() is True:
+                label.config(text=players[1] + "wins")
+            elif check_winner() == "Tie":
+                label.config(text="Tie")
+
+
+
 
 def check_winner():
     pass
