d2f9a14d7b69a0ac1d51fd6fd088545ba07ed82b
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2024-02-06 21:20:52 +0100

    change the color of buttons/label based on game situation; win/tie.

--- app.py
+++ app.py
@@ -3,22 +3,23 @@ import random
 
 def next_turn(row, column):
     global player
-    if buttons[row][column]["text"]  == "" and check_winner() is False:
-        if player == players[0]:
-            buttons[row][column]["text"] = player
-            if check_winner() is False:
-                player = players[1]
-                label.config(text=player[1] + " turn")
-            elif check_winner() is True:
-                label.config(text=players[0] + "wins")
-            elif check_winner() == "Tie":
-                label.config(text="Tie")
+    if buttons[row][column]["text"] == "" and check_winner() is False:
+        buttons[row][column]["text"] = player
+        if check_winner() is False:
+            player = players[1] if player == players[0] else players[0]
+            label.config(text=player + " turn")
+        elif check_winner() is True:
+            label.config(fg="#6cc4a0")
+            label.config(text=player + " wins")
+        elif check_winner() == "Tie":
+            label.config(text="Tie")
         else:
             buttons[row][column]["text"] = player
             if check_winner() is False:
                 player = players[0]
                 label.config(text=player[0] + " turn")
             elif check_winner() is True:
+                label.config(fg="#6cc4a0")
                 label.config(text=players[1] + "wins")
             elif check_winner() == "Tie":
                 label.config(text="Tie")
@@ -27,17 +28,33 @@ def check_winner():
     # check all the rows for a win
     for row in range(3):
         if buttons[row][0]["text"] == buttons[row][1]["text"] == buttons[row][2]["text"] != "" :
+            buttons[row][0].config(fg="#6cc4a0")
+            buttons[row][1].config(fg="#6cc4a0")
+            buttons[row][2].config(fg="#6cc4a0")
             return True
     # check all the columns for a win
     for column in range(3):
          if buttons[0][column]["text"] == buttons[1][column]["text"] == buttons[2][column]["text"] != "":
+            buttons[0][column].config(fg="#6cc4a0")
+            buttons[1][column].config(fg="#6cc4a0")
+            buttons[2][column].config(fg="#6cc4a0")
             return True
     # check diagonal win conditions
     if buttons[0][0]["text"] == buttons[1][1]["text"] == buttons[2][2]["text"] != "":
+        buttons[0][0].config(fg="#6cc4a0")
+        buttons[1][1].config(fg="#6cc4a0")
+        buttons[2][2].config(fg="#6cc4a0")
         return True
     elif buttons[0][2]["text"] == buttons[1][1]["text"] == buttons[2][0]["text"] != "":
+        buttons[0][2].config(fg="#6cc4a0")
+        buttons[1][1].config(fg="#6cc4a0")
+        buttons[2][0].config(fg="#6cc4a0")
         return True
     elif empty_spaces() is False:
+        for row in range(3):
+            for column in range(3):
+                label.config(fg="#d6d44b")
+                buttons[row][column].config(fg="#d6d44b")
         return "Tie"
     else:
         return False
