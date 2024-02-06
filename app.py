4e5ea29c8b59a35be184a9221f8c3ffa73cd033d
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2024-02-06 18:47:27 +0100

    finish empty_spaces function to check if there is any empty spaces left, or there is a Tie.

--- app.py
+++ app.py
@@ -44,7 +44,15 @@ def check_winner():
 
 
 def empty_spaces():
-    pass
+    spaces = 9
+    for row in range(3):
+        for column in range(3):
+            if buttons[row][column]["text"]  != "":
+                spaces -= 1
+    if spaces == 0:
+        return False
+    else:
+        return True
 
 def new_game():
     pass
